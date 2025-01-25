32acf5219de107ee3b81f9b38687a100bbc11369
Emily Lasecki <emilylasecki@oakland.edu>
2025-01-25 17:49:14 -0500

    altered method of displaying colors

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ document.getElementById("myDiv").innerHTML = "hello";
 class GridSystem {
 	constructor(matrix) {
 		this.matrix = matrix;
-		this.uiContext = this.#getContext(420, 580, "#000");
+		this.uiContext = this.#getContext(700, 700, "#000");  //put player score and info here
 		this.outlineContext = this.#getContext(0, 0, "#444");
 		this.topContext = this.#getContext(0, 0, "#111", true);
 		this.cellSize = 40;
@@ -13,12 +13,12 @@ class GridSystem {
 
 	#getCenter(w, h) {
 		return {
-			x: window.innerWidth / 2 - w / 2 + "px",
+			x: window.innerWidth / 2 - w / 2 + "px",  // center the canvas
 			y: window.innerHeight / 2 - h / 2 + "px"
 		};
 	}
 
-	#getContext(w, h, color = "#111", isTransparent = false) {
+	#getContext(w, h, color = "#111", isTransparent = false) {  //# makes this private not public
 		this.canvas = document.createElement("canvas");
 		this.context = this.canvas.getContext("2d");
 		this.width = this.canvas.width = w;
@@ -37,7 +37,7 @@ class GridSystem {
 	}
 
 	render() {
-		const w = (this.cellSize + this.padding) * this.matrix[0].length - (this.padding);
+		const w = (this.cellSize + this.padding) * this.matrix[0].length - (this.padding); // area on the outside: blackest black
 		const h = (this.cellSize + this.padding) * this.matrix.length - (this.padding);
 
 		this.outlineContext.canvas.width = w;
@@ -52,7 +52,20 @@ class GridSystem {
 
 		for (let row = 0; row < this.matrix.length; row ++) {
 			for (let col = 0; col < this.matrix[row].length; col ++) {
-				this.outlineContext.fillStyle = this.matrix[row][col] > 0 ? "#4488FF" : "#111";
+				//this.outlineContext.fillStyle = this.matrix[row][col] > 0 ? "#4488FF" : "#111"; // fill color based on status of the matrix 
+				//this.outlineContext.fillRect(col * (this.cellSize + this.padding),
+				//row * (this.cellSize + this.padding),
+				//this.cellSize, this.cellSize);
+				const cellVal = this.matrix[row][col];
+				let color = "#111"
+
+				if (cellVal === 1) {
+					color = "blue";
+				} else if (cellVal === 0){
+					color = "grey";
+				}
+
+				this.outlineContext.fillStyle = color;
 				this.outlineContext.fillRect(col * (this.cellSize + this.padding),
 				row * (this.cellSize + this.padding),
 				this.cellSize, this.cellSize);
@@ -61,11 +74,13 @@ class GridSystem {
 
 		this.uiContext.font = "20px Courier";
 		this.uiContext.fillStyle = "white";
-		this.uiContext.fillText("Nonogram Grid", 20, 30);
+		this.uiContext.fillText("Puzzle #1 - Fish", 20, 30);
+
+		// do check to see if the matricies are equal
 	}
 }
 
-const gridMatrix = [
+const solMatrix = [  // solution to the puzzle
 	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
 	[0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
 	[0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
@@ -78,5 +93,34 @@ const gridMatrix = [
     [0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
 ];
 
-const gridSystem = new GridSystem(gridMatrix);
+workingMatrix = [  // current input by the user
+	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+	[0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+	[0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    [0, 0, 0, 1, 1, 1, 1, 0, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+]
+class updateGrid {
+
+	changeCellState(workingMatrix) {  //can we just call render again? update the working matrix and pass it to render again.
+	this.matrix = workingMatrix
+	for (let row = 0; row < this.matrix.length; row ++) {
+		for (let col = 0; col < this.matrix[row].length; col ++) {
+			this.outlineContext.fillStyle = this.matrix[row][col] > 0 ? "#4488FF" : "#111"; // fill color based on status of the matrix 
+			this.outlineContext.fillRect(col * (this.cellSize + this.padding),
+			row * (this.cellSize + this.padding),
+			this.cellSize, this.cellSize);
+		}
+}
+}
+}
+
+//workingMatrix.addEventListener("onclick", changeCellState) 
+
+const gridSystem = new GridSystem(workingMatrix);
 gridSystem.render();
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: '.' });
 });
 
+app.use(express.static(path.join("."))); // gets other files that are needed from current directory
+
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
