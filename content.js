9874d0ecb6689ad59efe38de443d6f06efdb940b
Emily Lasecki <emilylasecki@oakland.edu>
2025-02-07 21:59:55 -0500

    organized buttons into a grid

--- content.js
+++ content.js
@@ -10,26 +10,37 @@ for (var i = 0; i <10; i++) {
 }*/
 
 const buttonNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-
+var br = document.createElement("br");
 for (var i =0; i<buttonNames.length; i++) {
-    const button = document.createElement("button");
-    button.id = "btnItems"
-    button.textContent = '.';
-    button.addEventListener('click', () => {
-        button.style.backgroundColor = "purple";
-    });
-    button.style.backgroundColor = "red";
+    document.body.appendChild(document.createElement("br"));  // only does this once lmao
+    for (var j=0; j<buttonNames.length; j++) {
+        const button = document.createElement("button");
+        button.id = "btnItems"
+        button.textContent = '.';
+        button.addEventListener('click', () => {
+            if (button.style.backgroundColor == "red") {
+                button.style.backgroundColor = "purple";
+                } else {
+                   button.style.backgroundColor="red";
+               }
+        });
+        button.style.backgroundColor = "red";
 
-    button.onclick = function(el) {
+        button.onclick = function(el) {
        // button.addEventListener('click', () => {
           //  button.style.backgroundColor = "purple";
        // });
-        const li = document.createElement('li');
-        li.textContent = items[i].name;
+            const li = document.createElement('li');
+            li.textContent = items[i].name;
     // append list element to your <ul> list 
         document.getElementById('list-section').appendChild(li);
     }
-    document.body.appendChild(button)
+    document.body.appendChild(button);
+}
+//var br = document.createTextNode("br");
+//element.appendChild(br);
+//document.write("<br>");
+//document.write("hello");
 }
 
 
