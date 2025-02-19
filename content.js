249a12bf76d2e9aefc6394a716a1b023131a9789
Emily Lasecki <emilylasecki@oakland.edu>
2025-02-19 15:51:54 -0500

    added getting rows x columns automatically and started win checker

--- content.js
+++ content.js
@@ -1,3 +1,16 @@
+// current issues:
+
+// make colors more appealing
+// make the border size and text size shift so that everything still looks nice on small screens
+// win condition checker
+// define imports so that multiple levels can be selected
+// come up with method to toggle between levels
+// readme
+// info on page about nonograms and how to play
+// level difficulty rating?
+// figure out how to get on itch.io
+
+
 document.body.style.backgroundColor = "#F8F8FF"//"#f0f8ff";
 
 
@@ -8,23 +21,23 @@ const solMatrix = [  // solution to the puzzle
     [0, 0, 0, 1, 1, 1, 1, 0, 1, 1],
     [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
     [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
     [0, 0, 0, 1, 1, 1, 0, 0, 1, 1],
     [0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
 ];
 
-workingMatrix = [  // current input by the user might not use
-	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-	[0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-	[0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    [0, 0, 0, 1, 1, 1, 1, 0, 1, 1],
-    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-    [0, 0, 0, 1, 1, 1, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+var workingMatrix = [  // current input by the user might not use
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
 rowClues  = [
@@ -55,52 +68,53 @@ colClues = [
 ]
 
 
-// systematically extract these numbers?
-rowCluesNum = 3;  //change this to the number of cols in row clues
-colCluesNum =2;
+
+colCluesNumber = colClues[0].length;
+rowCluesNumber = rowClues[0].length;
 
 document.getElementById("myDiv").innerHTML = "=ōwō=";
 const buttonNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 const column = document.getElementById("col2");
 const cluerow = document.getElementById("col1");
 
-for (i=0; i<rowCluesNum; i++){  // createboxes in the dead space between the 2 clues so everything lines up
+// createboxes in the dead space between the 2 clues so everything lines up
+for (i=0; i<colCluesNumber; i++){ 
     cluerow.appendChild(document.createElement("br"));
-    for (j=0; j<colCluesNum; j++) {
+    for (j=0; j<rowCluesNumber; j++) {
         const button = document.createElement("button");
         button.id = "blank" +j + '' + i;
         button.textContent = "X";
-        button.style.color= "grey";
-        button.style.backgroundColor="grey";
+        button.style.color= "#F8F8FF";
+        button.style.backgroundColor="#F8F8FF";
         button.classList.add("buttonInactive");
-        button.style.borderColor="grey";
+        button.style.borderColor="#F8F8FF";
         cluerow.appendChild(button);
     }
 }
 
 
-// column clues
+// row clues
 for (var m=0; m<10; m++) {
     cluerow.appendChild(document.createElement("br"));
-    for (var k =0; k<colCluesNum; k++) {
+    for (var k =0; k<rowCluesNumber; k++) {
         const button = document.createElement("button");
         button.id = "cluecol0" + k;
         button.textContent = rowClues[m][k];
         button.style.color= "black";
         if (button.textContent == "0") {
-            button.style.color = "grey";
+            button.style.color = "#F8F8FF";
         }
-        button.style.backgroundColor="grey";
+        button.style.backgroundColor="#F8F8FF";
         button.classList.add("buttonInactive");
-        button.style.borderColor="grey";
-        console.log(k);
+        button.style.borderColor="#F8F8FF";
+      //  console.log(k);
         //cluerow.appendChild(document.createElement("br"));
         cluerow.appendChild(button);
     }
 }
 
-//row clues
-for (var m=0; m<rowCluesNum; m++) {
+//column clues
+for (var m=0; m<colCluesNumber; m++) {
     column.appendChild(document.createElement("br"));
     for (var k =0; k<10; k++) {
         const button = document.createElement("button");
@@ -108,12 +122,12 @@ for (var m=0; m<rowCluesNum; m++) {
         button.style.color= "black";
         button.textContent = colClues[k][m];
         if (button.textContent == "0") {
-            button.style.color = "grey";
+            button.style.color = "#F8F8FF";
         }
-        button.style.backgroundColor="grey";
-        button.classList.add("colInactive");  //colInactive
-        button.style.borderColor="grey";
-        console.log(k);
+        button.style.backgroundColor="#F8F8FF";
+        button.classList.add("colInactive");
+        button.style.borderColor="#F8F8FF";
+        //console.log(k);
         //cluerow.appendChild(document.createElement("br"));
         column.appendChild(button);
     }
@@ -140,15 +154,25 @@ for (var i =0; i<buttonNames.length; i++) {
                 button.style.color= "black";
                 button.style.borderInlineColor = "black";
                 button.style.borderBlockColor = "black";
-                console.log(button.id);  // can use button.id to get index
+                var x = (button.id).slice(-1);
+                   var y = (button.id).slice(-2, -1);
+                   console.log(x + " " + y);
+                   workingMatrix[y][x] = 1;
+                   checkWin()
+               // console.log(button.id);  // can use button.id to get index
                 // get the index m and n for the matrix
                 } else if (button.style.backgroundColor == "black") {
+                   var x = (button.id).slice(-1);
+                   var y = (button.id).slice(-2, -1);
+                   console.log(x + " " + y);
+                   workingMatrix[y][x] = 0;
+                   checkWin()
                    button.style.backgroundColor="grey";
                    button.style.color= "black";
                    button.style.borderInlineColor = "grey";
                    button.style.borderBlockColor = "grey";
                } else {
-                   button.style.backgroundColor="darkgrey"; //SeaShell is a contender
+                   button.style.backgroundColor="darkgrey";
                    button.style.color= "darkgrey";
                    button.style.borderInlineColor = "darkgrey";
                    button.style.borderBlockColor = "darkgrey";
@@ -164,8 +188,16 @@ for (var i =0; i<buttonNames.length; i++) {
 }
 
 }
+function checkWin() {
+for (var i =0; i<buttonNames.length; i++) {
+    for (var j=0; j<buttonNames.length; j++) {
+        if (workingMatrix[i][j] != solMatrix[i][j]) {
+            console.log("still solving");
+        }
+    }
+    // do something here for when everything matches
+}
+}
 
-
-// do some loopy boi to display a lot of these friends and toggle the corresponding one
 // issue now is how to check for win status?
 // extract the index of each cell and compare to the solution
